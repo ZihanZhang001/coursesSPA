fb03dbed8f1aebd6a9b696afb4ca3b6b8a11675f
Zihan Zhang <769492047@qq.com>
2018-12-19 18:40:09 +0000

    delete home cypress

--- cypress/integration/aboutus.spec.js
+++ cypress/integration/aboutus.spec.js
@@ -1,15 +1,15 @@
-describe("About us", () => {
-
-  beforeEach(() => {
-    cy.visit("/");
-    // Click Donate navbar link
-    cy.get('.navbar-nav:nth-child(2)').
-    find('.nav-item:nth-child(1)').click()
-  });
-
-  it("Shows a header", () => {
-    cy.get(".vue-title").should('contain','About');
-  });
-
-
-});
+// describe("About us", () => {
+//
+//   beforeEach(() => {
+//     cy.visit("/");
+//     // Click Donate navbar link
+//     cy.get('.navbar-nav:nth-child(2)').
+//     find('.nav-item:nth-child(1)').click()
+//   });
+//
+//   it("Shows a header", () => {
+//     cy.get(".vue-title").should('contain','About');
+//   });
+//
+//
+// });
